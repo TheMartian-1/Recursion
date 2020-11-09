b35177de49aebad5a1f4f5ca8feaf8316444cba1
{Marshall} <{themarshallmash@gmail.com}>
2020-11-09 16:17:40 -0600

    First Commit

--- Recursion/src/Cake.java
+++ Recursion/src/Cake.java
@@ -1,4 +1,22 @@
 
 public class Cake {
-
+	private int bites = 10;
+	public Cake() {
+		// FIXME 
+	}
+	
+	public void takeBite() {
+		if(bites <= 0) {
+		System.out.println("No cake left");
+		}
+		else {
+			bites--;
+			System.out.println("tasty");
+		}
+		
+	}
+	
+	public boolean isEmpty() {
+		return bites == 0;
+	}	
 }
