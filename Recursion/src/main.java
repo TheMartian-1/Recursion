4973120bf30870ca676fdf7295f525531b63150b
{Marshall} <{themarshallmash@gmail.com}>
2020-11-09 16:32:31 -0600

    Version 1.2

--- Recursion/src/main.java
+++ Recursion/src/main.java
@@ -1,8 +1,18 @@
 
 public class main {
 
+	public static void eatCake(Cake cake) {
+		if(!cake.isEmpty()) {
+		cake.takeBite();
+		eatCake(cake);
+		}
+		return;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Cake cake = new Cake();
+		eatCake(cake);
+		System.out.println("Done eating cake. Time to work!");
 
 	}
 
